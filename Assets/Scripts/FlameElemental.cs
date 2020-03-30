54d8521ad60028186762bc308b7a40bccfc78652
VacatedSum <59126285+VacatedSum@users.noreply.github.com>
2020-03-30 15:28:31 -0400

    Worked on Unit.cs, created FlameElemental.cs

---
+++ Assets/Scripts/FlameElemental.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlameElemental : Unit
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        unitName = "Flame Elemental";
+        unitLevel = 1;
+        damage = 2;
+        maxHP = 10;
+        currentHP = maxHP;
+        experience = unitLevel * unitLevel;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (this.currentHP <= 0){
+            Destroy(this);
+        }
+    }
+}
--- Assets/Scripts/Unit.cs
+++ Assets/Scripts/Unit.cs
@@ -1,24 +1,53 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Unit : MonoBehaviour
+public abstract class Unit : MonoBehaviour
 {
+	public int experience;
     public string unitName;
 	public int unitLevel;
 	
+	//What is this variable for?
+	//Assuming it's more like "strength" or "attack"
 	public int damage;
 	
 	public int maxHP;
 	public int currentHP;
-	
-	public bool TakeDamage(int dmg)
+	/**
+	* Reduces HP of unit.
+	* Returns true if unit dies as a result.
+	* @param dmg - (integer) damage taken
+	*/
+	public int TakeDamage(int dmg)
 	{
 		currentHP -= dmg;
 		
-		if(currentHP <= 0)
-			return true;
-		else
-			return false;
+		//Update will take care of removing the bugger when dead.
+		//This can be chain called
+		//Player.experience += Critter.TakeDamage(Player.Attack());
+		
+		if(currentHP <= 0) {
+			//If critter dies, return experience.
+			return experience;
+		}
+		else {
+			//Otherwise return 0
+			return 0;
+		}
+		
+	}
+	/**
+	* Returns a random number between 0 and 'damage' variable
+	*/
+	public int Attack(){
+		System.Random RNG = new System.Random();
+		return RNG.Next(0, damage);
+	}
+	void update(){
+		if (this.currentHP <= 0){
+			Destroy(this);
+		}
 	}
 }
