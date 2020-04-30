3730a15ba189bae8c7f2c63a85a3fbccd71d896d
Firemission1343 <libertyv13@yahoo.com>
2020-04-29 22:55:05 -0400

    MainMenu + Release build v.01
    Added MainMenu
    Fixed Canvas issue
    Fixed Resolution bug
    Fixed Button Glitch
    **Restructured Scenes and Build Package

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+      SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); //loads next scene from the menu to play...Note: requires to "build" to work.
+        //SceneManager.LoadScene("level_0");
+    }
+
+    public void QuitGame()
+    {
+      Application.Quit();
+    }
+}
